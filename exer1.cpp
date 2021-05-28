631b2525efefc094a9ece0f9effa92c540eac60c
cyber-anonimo <84997511+cyber-anonimo@users.noreply.github.com>
2021-05-28 19:52:34 -0300

    Add files via upload

---
+++ exer1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num1 = 0;
+	cout<< "digite o primeiro numero: \n";
+	cin >> num1;
+	
+	if(num1 > 20){
+		cout << num1;
+	}
+	
+	
+	
+    //esse comando faz a tela ficar esperando alguma tela
+	system("pause");	
+	return 0;
+}
